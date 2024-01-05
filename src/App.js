56d9ca671859bab723203832646c207473996ace
hayden.poon <hayden.poon@fdmgroup.com>
2024-01-05 15:59:04 +0000

    first message

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import React, { Component }  from 'react';
+
+
+export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = "x";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </div>
+  );
+}
+
+function Square({value, onSquareClick}) {
+
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
