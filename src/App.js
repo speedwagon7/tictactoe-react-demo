29a9fffb9af33ad127b38035d518d7917156f50b
hayden.poon <hayden.poon@fdmgroup.com>
2024-01-10 00:40:45 +0000

    adding game history

--- src/App.js
+++ src/App.js
@@ -12,11 +12,8 @@ function Square({value, onSquareClick}) {
 }
 
 
-export default function Board() {
+function Board({xIsNext, squares, onPlay}) {
 
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
   //display winner or next move
   const winner = calculateWinner(squares)
@@ -46,8 +43,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O"
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   return (
@@ -91,4 +87,46 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+export default function Game(){
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length -1];
+
+  function handlePlay(nextSquares){
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext)
+  }
+
+  function jumpTo(nextMove){
+    //to do
+    
+  };
+
+  const moves = history.map((squares,move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    }
+    else{
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}> {description}</button>
+      </li>
+    )
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>   
+      <div className="game-info">
+        <ol> {moves}</ol>
+      </div>
+    </div>
+  );
 }
